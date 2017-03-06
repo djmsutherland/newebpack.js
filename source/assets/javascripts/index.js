fcb25d76ddf017551232b75cb23cf3cdcf1c3fae
Mitch Gavan <mitchgavan@gmail.com>
2017-03-06 11:35:00 +1100

    converted sui-select to vue component

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,8 +2,9 @@ import "../stylesheets/index.css"
 
 // Components
 import "./components/sui-alert";
-import "./components/sui-switch";
 import "./components/sui-input";
+import "./components/sui-select";
+import "./components/sui-switch";
 
 import Store from "./services/store";
 import Vue from "vue";
