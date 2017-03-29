09e46e00ca3417292ff6437fe783c255d1c04a94
Sutherland, Dan <Dan.Sutherland@sensis.com.au>
2017-03-29 11:56:52 +1100

    Added code coverage to project
    - installed karma-coverage
    - installed istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,31 @@
 const env = { test: true }
 const webpack = require("./webpack.config")(env)
-const glob = "source/javascripts/**/*_spec.js"
+const specs = "source/javascripts/**/*_spec.js"
+const source = "source/javascripts/**/*!(spec).js"
+
+process.env.BABEL_ENV = "test"
 
 module.exports = function(config) {
   config.set({
     basePath: "",
     frameworks: ["mocha", "chai"],
-    files: [glob],
+    files: [specs, source],
     preprocessors: {
-      [glob]: ["webpack"]
+      [specs]: ["webpack"],
+      [source]: ["webpack"]
     },
     webpack,
     webpackMiddleware: {
       noInfo: true
     },
-    reporters: ["progress"],
+    reporters: ["progress", "coverage"],
+    coverageReporter: {
+      reporters: [
+        {type: "lcov", dir: "coverage/", subdir: "."},
+        {type: "json", dir: "coverage/", subdir: "."},
+        {type: "text-summary"}
+      ]
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
